57bcedb4fc9a9a27d92008beaeea8c01478a39a0
Brian Bruce <public1@brianbruce.org>
2019-03-19 00:40:50 -0400

    Updates

---
+++ react-app/precache-manifest.f791e3c06644cece4a7d27d8392655be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.61045775.js"
+  },
+  {
+    "revision": "3e806bb324237aded4ef",
+    "url": "/react-app/static/js/main.6a5a2f81.chunk.js"
+  },
+  {
+    "revision": "f6422e6e88cfb1ba2ef6",
+    "url": "/react-app/static/js/2.372fd93a.chunk.js"
+  },
+  {
+    "revision": "3e806bb324237aded4ef",
+    "url": "/react-app/static/css/main.0843d9c8.chunk.css"
+  },
+  {
+    "revision": "79d39031cf60be2addceac72b1de984a",
+    "url": "/react-app/index.html"
+  }
+];
